cd65e376407c5d383a86cd9a51f0adbcb1ad6f19
RAGHU440 <97328032+NitroXGAMER@users.noreply.github.com>
2023-08-01 15:15:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+x = 0;
+y = 0;
+screen_width = window.innerWidth;
+screen_height = window.innerHeight;
+draw_apple = "apple.png";
+
+var speak_data = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+    to_number = Number(content);
+    if(Number.isInteger(to_number) == true){
+      document.getElementById("apple.png")
+    }
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+
+function preload(){
+  loadImage("apple.png")
+}
